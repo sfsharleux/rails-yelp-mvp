223d1fd994cb23c62f09b19af28fa989d7edee19
Sarah Harleux <s.harleux@gmail.com>
2020-08-12 11:12:52 +0200

    Rails new with frontend and form gems

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,19 @@
+class RestaurantsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def createedit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+  get 'restaurants/new'
+  get 'restaurants/createedit'
+  get 'restaurants/show'
+  get 'restaurants/update'
+  get 'restaurants/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
